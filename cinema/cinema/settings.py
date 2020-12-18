60ae29c3fe54cd53d26e00a8365b10fede34d435
Bartosz Dobrosielski <dobry1@pm.me>
2020-12-18 14:22:37 +0100

    install django-shopping-cart

--- cinema/cinema/settings.py
+++ cinema/cinema/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'index'
+LOGIN_URL = 'login'
