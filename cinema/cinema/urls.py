b1fdc8d10902bf3be50f17ae328b37e2b2ce88b3
Bartosz Dobrosielski <dobry1@pm.me>
2020-12-18 14:58:54 +0100

    add user template to login/register

--- cinema/cinema/urls.py
+++ cinema/cinema/urls.py
@@ -33,3 +33,8 @@ urlpatterns = [
         path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
         path('newuser/', user_views.newuser, name='newuser'),
 ]
+
+# for media files
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
