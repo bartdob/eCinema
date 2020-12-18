f7102d2e4d31691ff588f17e47614f00b89380da
Bartosz Dobrosielski <dobry1@pm.me>
2020-12-18 13:32:40 +0100

    add to user - model, urls, forms, singnals

--- cinema/cinema/urls.py
+++ cinema/cinema/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
+from cart import views
 
 urlpatterns = [
         path('', include('shop.urls')),
         path('admin/', admin.site.urls),
+        path('register/', user_views.register, name='register'),
+        path('profile/', user_views.profile, name='profile'),
+        path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+        path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+        path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+        path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+        path('password_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+        path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+        path('newuser/', user_views.newuser, name='newuser'),
 ]
