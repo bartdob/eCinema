a8fda069792af36be172cf4f522612f6c5bbe456
Bartosz Dobrosielski <dobry1@pm.me>
2020-12-18 15:29:39 +0100

    add templates to user account

--- cinema/cart/views.py
+++ cinema/cart/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from shop.models import Movie
+from django.contrib.auth.decorators import login_required
+from cart.cart import Cart
 
-# Create your views here.
+@login_required()
+def cart_add(request, id):
+    cart = Cart(request)
+    product = Movie.objects.get(id=id)
+    cart.add(product=product)
+    return redirect("index")
+
+
+@login_required()
+def cart_detail(request):
+    return render(request, 'cart_detail.html')
